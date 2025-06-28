f8dbb105c25e64991048449a53e81a11bc5562a1
Pabloguevara10 <pabloguevarae2020@gmail.com>
2025-06-28 15:28:17 -0400

    Create TipoPokemon.java

---
+++ TipoPokemon.java
@@ -0,0 +1,19 @@
+package com.pokemon.modelos.pokemon;
+
+import java.io.Serializable;
+
+public enum TipoPokemon implements Serializable {
+    AGUA,
+    FUEGO,
+    PLANTA,
+    ELECTRICO,
+    NORMAL,
+    BICHO,
+    VOLADOR,
+    VENENO,
+    TIERRA,
+    ROCA,
+    PSIQUICO,
+    FANTASMA,
+    LUCHA;
+}
